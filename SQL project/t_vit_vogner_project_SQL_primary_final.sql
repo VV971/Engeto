31cd8ce0c300ca4b99d5d3823b230766d6a47766
VV971 <vit.vogner2@gmail.com>
2024-12-01 13:00:43 +0100

    Data for 2nd SQL query added into primary final table.

--- SQL project/t_vit_vogner_project_SQL_primary_final.sql
+++ SQL project/t_vit_vogner_project_SQL_primary_final.sql
@@ -1,24 +1,3 @@
-/*
-CREATE TABLE IF NOT EXISTS t_vit_vogner_project_SQL_primary_final AS (
-SELECT 	
-	cp.payroll_year,
-	cpib.name AS industry_branch,
-	cpvt.name AS pay_name,
-	AVG(cp.value) AS average_pay,
-	cpu.name AS currency
-FROM engeto_26_09_2024.czechia_payroll AS cp
-LEFT JOIN engeto_26_09_2024.czechia_payroll_value_type AS cpvt
-ON cp.value_type_code = cpvt.code
-LEFT JOIN engeto_26_09_2024.czechia_payroll_unit AS cpu
-ON cp.unit_code = cpu.code
-LEFT JOIN engeto_26_09_2024.czechia_payroll_industry_branch AS cpib
-ON cp.industry_branch_code = cpib.code
-WHERE cpvt.code = 5958 AND cpib.code IS NOT NULL 
-GROUP BY cp.payroll_year, cpib.name
-ORDER BY cp.payroll_year, cpib.name
-);
-*/
-
 CREATE TABLE IF NOT EXISTS t_vit_vogner_project_SQL_primary_final AS (
 WITH cte_payroll AS (
     SELECT  
