35564c10382e9e3972b65c1cb5a704afde8acba2
VV971 <vit.vogner2@gmail.com>
2024-12-03 21:19:12 +0100

    1st SQL query corrected for last changes in the primary final table and 3rd SQL query started.

--- SQL project/Engeto_SQL project_Query 1.sql
+++ SQL project/Engeto_SQL project_Query 1.sql
@@ -2,29 +2,29 @@
 
 WITH cte_pay_change AS (
 	SELECT 
-		tvv.payroll_year,
-		tvv.industry_branch,
-		tvv.pay_name,
-		tvv.average_pay,
+		tvvpspf.`year`,
+		tvvpspf.data_name AS industry_branch,
+		tvvpspf.data_type AS pay_name,
+		tvvpspf.average_value AS average_pay,
 		CASE
-			WHEN LAG(tvv.average_pay) OVER (PARTITION BY tvv.industry_branch ORDER BY tvv.payroll_year) IS NULL THEN 'Missing Data'
-			WHEN tvv.average_pay > (
-				SELECT tvv2.average_pay
-				FROM engeto_26_09_2024.t_vit_vogner_project_sql_primary_final AS tvv2
-				WHERE tvv2.payroll_year = tvv.payroll_year - 1
-				AND tvv2.industry_branch = tvv.industry_branch
-				GROUP BY tvv2.industry_branch
+			WHEN LAG(tvvpspf.average_value) OVER (PARTITION BY tvvpspf.data_name ORDER BY tvvpspf.`year`) IS NULL THEN 'Missing Data'
+			WHEN tvvpspf.average_value > (
+				SELECT tvvpspf2.average_value
+				FROM engeto_26_09_2024.t_vit_vogner_project_sql_primary_final AS tvvpspf2
+				WHERE tvvpspf2.`year` = tvvpspf.`year` - 1
+				AND tvvpspf2.data_name = tvvpspf.data_name
+				GROUP BY tvvpspf2.data_name
 				) THEN 'Rising'
 			ELSE 'Decreasing'
 		END AS yty_pay_trend,
-		LAG(tvv.average_pay) OVER (PARTITION BY tvv.industry_branch ORDER BY tvv.payroll_year) AS average_pay_previous_year,
-		tvv.average_pay - LAG(tvv.average_pay) OVER (PARTITION BY tvv.industry_branch ORDER BY tvv.payroll_year) AS average_yty_pay_change_abs,
-		tvv.average_pay / LAG(tvv.average_pay) OVER (PARTITION BY tvv.industry_branch ORDER BY tvv.payroll_year) AS average_yty_pay_change_percentage
-	FROM engeto_26_09_2024.t_vit_vogner_project_sql_primary_final AS tvv
-	GROUP BY tvv.payroll_year, tvv.industry_branch
+		LAG(tvvpspf.average_value) OVER (PARTITION BY tvvpspf.data_name ORDER BY tvvpspf.`year`) AS average_pay_previous_year,
+		tvvpspf.average_value - LAG(tvvpspf.average_value) OVER (PARTITION BY tvvpspf.data_name ORDER BY tvvpspf.`year`) AS average_yty_pay_change_abs,
+		tvvpspf.average_value / LAG(tvvpspf.average_value) OVER (PARTITION BY tvvpspf.data_name ORDER BY tvvpspf.`year`) AS average_yty_pay_change_percentage
+	FROM engeto_26_09_2024.t_vit_vogner_project_sql_primary_final AS tvvpspf
+	GROUP BY tvvpspf.`year`, tvvpspf.data_name
 )
 SELECT 
-	cte_pc.payroll_year,
+	cte_pc.`year` AS payroll_year,
 	cte_pc.industry_branch,
 	cte_pc.pay_name,
 	ROUND(cte_pc.average_pay, 2) AS average_pay,
@@ -33,4 +33,4 @@ SELECT
 	ROUND(cte_pc.average_yty_pay_change_abs, 2) AS average_yty_pay_change_abs,
 	ROUND((cte_pc.average_yty_pay_change_percentage * 100) - 100, 3) AS average_yty_pay_change_percentage
 FROM cte_pay_change AS cte_pc
-GROUP BY cte_pc.payroll_year, cte_pc.industry_branch;
+GROUP BY cte_pc.`year`, cte_pc.industry_branch;
--- SQL project/Engeto_SQL project_Query 3.sql
+++ SQL project/Engeto_SQL project_Query 3.sql
@@ -1,4 +1,64 @@
 /*
+Primární tabulky:
+
+czechia_payroll – Informace o mzdách v různých odvětvích za několikaleté období. Datová sada pochází z Portálu otevřených dat ČR.
+czechia_payroll_calculation – Číselník kalkulací v tabulce mezd.
+czechia_payroll_industry_branch – Číselník odvětví v tabulce mezd.
+czechia_payroll_unit – Číselník jednotek hodnot v tabulce mezd.
+czechia_payroll_value_type – Číselník typů hodnot v tabulce mezd.
+czechia_price – Informace o cenách vybraných potravin za několikaleté období. Datová sada pochází z Portálu otevřených dat ČR.
+czechia_price_category – Číselník kategorií potravin, které se vyskytují v našem přehledu.
+Číselníky sdílených informací o ČR:
+
+czechia_region – Číselník krajů České republiky dle normy CZ-NUTS 2.
+czechia_district – Číselník okresů České republiky dle normy LAU.
+Dodatečné tabulky:
+
+countries - Všemožné informace o zemích na světě, například hlavní město, měna, národní jídlo nebo průměrná výška populace.
+economies - HDP, GINI, daňová zátěž, atd. pro daný stát a rok.
+
 Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 */
+SELECT
+    cp.category_code,
+    cpc.name,
+    -- cp.id,
+    cp.value,
+    cp.date_from,
+    cpc.price_value,
+    cpc.price_unit 
+FROM engeto_26_09_2024.czechia_price AS cp
+LEFT JOIN engeto_26_09_2024.czechia_price_category AS cpc 
+ON cp.category_code = cpc.code
+WHERE cp.category_code = '111101'
+ORDER BY cp.date_from ASC;
 
+SELECT
+    cp.category_code,
+    cpc.name,
+    -- cp.id,
+    ROUND(AVG(cp.value), 2) AS average_value,
+    cp.date_from,
+    cpc.price_value,
+    cpc.price_unit 
+FROM engeto_26_09_2024.czechia_price AS cp
+LEFT JOIN engeto_26_09_2024.czechia_price_category AS cpc 
+ON cp.category_code = cpc.code
+WHERE cp.category_code = '111101'
+GROUP BY 
+    cp.category_code,
+    cpc.name;
+
+WITH cte_food_prices AS (
+    SELECT
+        tvvpspf.`year`,
+        tvvpspf.code AS food_code,
+        tvvpspf.data_name AS food_name,
+        tvvpspf.average_value AS average_food_value,
+        tvvpspf.unit AS food_unit
+    FROM engeto_26_09_2024.t_vit_vogner_project_sql_primary_final AS tvvpspf
+    WHERE data_type = 'Průměrná cena za jednotku';
+)
+
+SELECT *
+FROM engeto_26_09_2024.t_vit_vogner_project_sql_primary_final AS tvvpspf;
