079c5eacfc8de759fdcd10ae102d24a52b8e08fb
VV971 <vit.vogner2@gmail.com>
2024-12-04 20:57:58 +0100

    3rd SQL query successsfully tested.

--- SQL project/Engeto_SQL project_Query 3.sql
+++ SQL project/Engeto_SQL project_Query 3.sql
@@ -19,46 +19,31 @@ economies - HDP, GINI, daňová zátěž, atd. pro daný stát a rok.
 
 Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 */
-SELECT
-    cp.category_code,
-    cpc.name,
-    -- cp.id,
-    cp.value,
-    cp.date_from,
-    cpc.price_value,
-    cpc.price_unit 
-FROM engeto_26_09_2024.czechia_price AS cp
-LEFT JOIN engeto_26_09_2024.czechia_price_category AS cpc 
-ON cp.category_code = cpc.code
-WHERE cp.category_code = '111101'
-ORDER BY cp.date_from ASC;
 
-SELECT
-    cp.category_code,
-    cpc.name,
-    -- cp.id,
-    ROUND(AVG(cp.value), 2) AS average_value,
-    cp.date_from,
-    cpc.price_value,
-    cpc.price_unit 
-FROM engeto_26_09_2024.czechia_price AS cp
-LEFT JOIN engeto_26_09_2024.czechia_price_category AS cpc 
-ON cp.category_code = cpc.code
-WHERE cp.category_code = '111101'
-GROUP BY 
-    cp.category_code,
-    cpc.name;
 
 WITH cte_food_prices AS (
     SELECT
         tvvpspf.`year`,
         tvvpspf.code AS food_code,
         tvvpspf.data_name AS food_name,
-        tvvpspf.average_value AS average_food_value,
+        tvvpspf.average_value AS average_food_price,
+        tvvpspf.average_value / LAG(tvvpspf.average_value) OVER (PARTITION BY tvvpspf.data_name ORDER BY tvvpspf.`year`) AS average_yty_food_price_change_percentage,
         tvvpspf.unit AS food_unit
     FROM engeto_26_09_2024.t_vit_vogner_project_sql_primary_final AS tvvpspf
-    WHERE data_type = 'Průměrná cena za jednotku';
+    WHERE data_type = 'Průměrná cena za jednotku'
 )
-
-SELECT *
-FROM engeto_26_09_2024.t_vit_vogner_project_sql_primary_final AS tvvpspf;
+SELECT DISTINCT
+    -- cte_fp.`year`,
+    cte_fp.food_code,
+    cte_fp.food_name,
+    -- cte_fp.average_food_price,
+    ROUND(SUM(cte_fp.average_yty_food_price_change_percentage), 4) AS average_food_price_percentage
+    -- cte_fp.food_unit
+FROM cte_food_prices AS cte_fp
+GROUP BY 
+    -- cte_fp.`year`,
+    cte_fp.food_code,
+    cte_fp.food_name
+ORDER BY 
+    average_food_price_percentage ASC
+LIMIT 2;
